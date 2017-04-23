ec990437e099537440f171738c7a1626a82f66ff
eneas <emacias@ces.vocento.com>
2017-04-23 21:26:03 +0200

    registration frontend done

--- src/AppBundle/Form/RegistrationClientFormType.php
+++ src/AppBundle/Form/RegistrationClientFormType.php
@@ -4,6 +4,8 @@ namespace AppBundle\Form;
 
 use AppBundle\Entity\User\Client;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +14,18 @@ class RegistrationClientFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
+                    ->add("username")
                     ->add("name")
                     ->add("surname")
-                    ->add("dni");
+                    ->add("dni")
+                    ->add('plainPassword', RepeatedType::class, array(
+                            'type' => PasswordType::class,
+                            'invalid_message' => 'The password fields must match.',
+                            'options' => array('attr' => array('class' => 'password-field')),
+                            'required' => true,
+                            'first_options'  => array('label' => 'Password'),
+                            'second_options' => array('label' => 'Repeat Password')))
+                    ->add('email');
     }
 
     public function configureOptions(OptionsResolver $resolver)
